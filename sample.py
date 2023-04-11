e927d712ee7945c89ad98adcd29bbdd12af20af0
Aishwarya-123-afk <62552617+Aishwarya-123-afk@users.noreply.github.com>
2023-04-11 23:03:51 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,1 +1,8 @@
-print("hello world")
+import requests
+
+output = requests.get("http://localhost:8080/job/python_script/lastBuild/consoleText")
+print(output.text)
+
+filepath = ".github/pull_request_template.md"
+with open(filepath,'a', newline = "") as fp:
+    fp.write(output.text)
