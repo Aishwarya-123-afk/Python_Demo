d4ed8b25d93b711203d7cd1c49e57c0a1f6fb70f
Aishwarya-123-afk <62552617+Aishwarya-123-afk@users.noreply.github.com>
2023-05-06 21:13:37 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -3,8 +3,8 @@ import requests
 output = requests.get("http://localhost:8080/job/python_script/lastBuild/consoleText")
 print(output.text)
 
- github_pulls = requests.get(https://api.github.com/repos/Aishwarya-123-afk/Python_Demo/pulls)
- print("github_pulls")
+github_pulls = requests.get(https://api.github.com/repos/Aishwarya-123-afk/Python_Demo/pulls)
+print("github_pulls")
 
 filepath = ".github/pull_request_template.md"
 with open(filepath,'a', newline = "") as fp:
