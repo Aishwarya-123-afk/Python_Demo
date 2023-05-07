d1e15fc78e4afa968ff93b0d8252c5db1cb2e2b0
Aishwarya-123-afk <62552617+Aishwarya-123-afk@users.noreply.github.com>
2023-05-07 19:03:09 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -6,9 +6,9 @@
 # github_pulls = requests.get("https://api.github.com/repos/Aishwarya-123-afk/Python_Demo/pulls")
 # print("github_pulls")
 
-# filepath = ".github/pull_request_template.md"
-# with open(filepath,'a', newline = "") as fp:
-#     fp.write(output.text)
+# # filepath = ".github/pull_request_template.md"
+# # with open(filepath,'a', newline = "") as fp:
+# #     fp.write(output.text)
 
 import subprocess
 
